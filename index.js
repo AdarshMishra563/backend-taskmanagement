37e6cee3890b5ca6a54d39311924965c9b864d80
AdarshMishra563 <am0070563@gmail.com>
2025-05-08 21:36:41 +0530

    video call

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const db = require("./model/db");
 db();
 const User=require('./model/User')
+const http = require("http");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 app.use(cors());
+const server = http.createServer(app);
 app.use(express.json());
 const authRoutes=require('./routes/authRoutes');
 app.use("/api/auth",authRoutes);
-const PORT=4000 || 5000;
 
+const socketHandler=require("./controllers/VideoController")
+const PORT=4000 || 5000;
+socketHandler(server);
 
 
 app.listen(PORT,()=>console.log(`Server is running on ${PORT}`))
--- server.js
+++
@@ -1,7 +0,0 @@
-const sendEmail=require('./sendEmail');
-require('dotenv').config();
-const send=async()=>{
-    const data =await sendEmail("godsadarsh@gmail.com","nsnn","otp");
-    console.log(data)
-}
-send()

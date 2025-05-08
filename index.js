8f6633a7c3df2ef9de5c3a0df2a0beb66481a9a8
AdarshMishra563 <am0070563@gmail.com>
2025-05-08 22:03:10 +0530

    a

--- index.js
+++ index.js
@@ -12,9 +12,70 @@ app.use(express.json());
 const authRoutes=require('./routes/authRoutes');
 app.use("/api/auth",authRoutes);
 
-const socketHandler=require("./controllers/VideoController")
+
 const PORT=4000 || 5000;
-socketHandler(server);
 
 
-app.listen(PORT,()=>console.log(`Server is running on ${PORT}`))
+
+
+
+
+const { Server } = require("socket.io");
+
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+
+
+
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+let users = {}; // Keeps track of connected users by userId: socketId
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("joinRoom", (userId) => {
+    users[userId] = socket.id;  // Add the user to the users object
+    socket.emit("yourID", socket.id);  // Emit the user's socket id
+
+    // Emit the updated list of online users to all clients
+    io.emit("onlineUsers", Object.keys(users));  // Emit list of online user ids
+  });
+
+  socket.on("callUser", (data) => {
+    const { from, to, signal } = data;
+    const toSocketId = users[to];
+    if (toSocketId) {
+      io.to(toSocketId).emit("incomingCall", { from, signal });
+    }
+  });
+
+  socket.on("answerCall", (data) => {
+    const { to, signal } = data;
+    const toSocketId = users[to];
+    if (toSocketId) {
+      io.to(toSocketId).emit("callAnswered", { signal });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    // Find the user who disconnected and remove them from the users object
+    for (let [userId, socketId] of Object.entries(users)) {
+      if (socketId === socket.id) {
+        delete users[userId];  // Remove user from the online list
+        break;
+      }
+    }
+
+    // Emit the updated list of online users after someone disconnects
+    io.emit("onlineUsers", Object.keys(users));
+  });
+});
+
+
+server.listen(PORT,()=>console.log(`Server is running on ${PORT}`))

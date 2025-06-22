77cac99974673708b91fcc6d02bece81db61e0f7
Adarsh Mishra <am0070563@gmail.com>
2025-06-22 15:53:05 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,7 +61,7 @@ exports.login = async (req, res) => {
       if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
   
       const payload = { user: { id: user.id } };
-      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1s" });
   
       res.json({ token,user });
     } catch (err) {

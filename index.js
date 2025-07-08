0d57b5e7f7f32e612776d332856e6909ab3d0ad6
Adarsh Mishra <am0070563@gmail.com>
2025-07-08 13:03:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const io = new Server(server, {
 
 
 const users = {};
+const editingTasks = {};
 
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
@@ -78,13 +79,31 @@ io.on("connection", (socket) => {
         io.to(targetSocketId).emit("receiveIceCandidate", { candidate });
       }
     });
-  
+  socket.on("startEditingTask", ({ taskId, userName }) => {
+  editingTasks[taskId] = userName;
+  io.emit("taskEditingStatus", { taskId, editingBy: userName });
+  console.log(`Task ${taskId} is being edited by ${userName}`);
+});
+
+socket.on("stopEditingTask", ({ taskId }) => {
+  delete editingTasks[taskId];
+  io.emit("taskEditingStatus", { taskId, editingBy: null });
+  console.log(`Task ${taskId} edit released`);
+});
+
     socket.on("disconnect", () => {
       if (socket.userId) {
         delete users[socket.userId];
         io.emit("onlineUsers", Object.keys(users));
         console.log("User disconnected:", socket.userId);
+      };
+
+       for (const [taskId, userName] of Object.entries(editingTasks)) {
+      if (users[socket.userId] === userName) {
+        delete editingTasks[taskId];
+        io.emit("taskEditingStatus", { taskId, editingBy: null });
       }
+    };
     });
   });
   
